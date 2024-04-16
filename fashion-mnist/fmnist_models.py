e04be5d2f8242346184ed99f7294c0c28a323d89
Jordan <zjzhang325@gmail.com>
2024-04-15 18:52:01 -0700

    Saving FMNIST work

--- fashion-mnist/fmnist_models.py
+++ fashion-mnist/fmnist_models.py
@@ -13,18 +13,16 @@ import numpy as np
 
 
 class MLP(nn.Module):
-    def __init__(self, d_in, d_hidden, d_out, n_hidden=1, dropout=0.1):
+    def __init__(self, d_in, d_hidden, d_out, n_hidden=2, dropout=0.1):
         super().__init__()
         self.input_linear = nn.Linear(d_in, d_hidden)
+        layers = []
+        for _ in range(n_hidden):
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout))
+            layers.append(nn.Linear(d_hidden, d_hidden))
         self.layers = nn.Sequential(
-            *(
-                [
-                    nn.ReLU(),
-                    nn.Dropout(dropout),
-                    nn.Linear(d_hidden, d_hidden),
-                ]
-                * n_hidden
-            )
+            *layers,
         )
         self.output_layer = nn.Sequential(
             nn.ReLU(),
@@ -46,17 +44,30 @@ class MLP(nn.Module):
 
 
 class CNN(nn.Module):
-    def __init__(self):
+    def __init__(self, d_out=10):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.conv1 = nn.Conv2d(1, 64, 3, 1)
+        self.max_pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(64, 128, 3, 1)
+        self.max_pool2 = nn.MaxPool2d(2)
+        self.dropout = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(3200, d_out)
 
-    def forward(self, x):
-        pass
+    def forward(self, pixel_values, labels=None):
+        x = self.conv1(pixel_values)
+        x = torch.relu(x)
+        x = self.max_pool1(x)
+        x = self.conv2(x)
+        x = torch.relu(x)
+        x = self.max_pool2(x)
+        x = torch.flatten(x, 1)
+        x = self.dropout(x)
+        x = self.fc1(x)
+        preds = torch.softmax(x, dim=-1)
+        if labels is not None:
+            loss = nn.CrossEntropyLoss()(preds, labels)
+            return (loss, preds)
+        return (preds,)
 
 
 accuracy = evaluate.load("accuracy")
@@ -71,6 +82,8 @@ def compute_metrics(eval_pred):
 
 def train(model_cls, dataset, train_args, **model_params):
     model = model_cls(**model_params)
+    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"Total trainable parameters: {pytorch_total_params}")
     data_collator = DefaultDataCollator()
 
     trainer = Trainer(
@@ -82,3 +95,4 @@ def train(model_cls, dataset, train_args, **model_params):
         compute_metrics=compute_metrics,
     )
     trainer.train()
+    return trainer
