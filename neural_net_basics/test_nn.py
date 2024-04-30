d0a2508cae6ea46b5f2057311225e32a3e22af6a
Jordan <zjzhang325@gmail.com>
2024-04-29 20:44:24 -0700

    More tests

--- neural_net_basics/test_nn.py
+++ neural_net_basics/test_nn.py
@@ -23,30 +23,15 @@ def helper_test_binary_op(op_name, a=None, b=None):
     op_func = getattr(at, op_name)
     st = op_func(bt)
     st.backward()
-    assert np.array_equal(st.value, op_func_np(b))
+    assert np.allclose(st.value, op_func_np(b), rtol=1e-15, atol=1e-20)
     apt = torch.tensor(a, requires_grad=True)
     bpt = torch.tensor(b, requires_grad=True) if isinstance(b, np.ndarray) else b
     op_func_pt = getattr(apt, op_name)
     spt = op_func_pt(bpt)
     spt.sum().backward()
-    assert np.array_equal(apt.grad.numpy(), at.grad)
+    assert np.allclose(apt.grad.numpy(), at.grad, rtol=1e-15, atol=1e-20)
     if isinstance(b, np.ndarray):
-        assert np.array_equal(bpt.grad.numpy(), bt.grad)
-
-
-def helper_test_unary_op(op_name, a=None):
-    a = a if a is not None else np.random.random([2, 3, 4])
-    op_func_np = getattr(np, op_name)
-    at = nn.Tensor(a)
-    op_func = getattr(at, op_name)
-    st = op_func()
-    st.backward()
-    assert np.array_equal(st.value, op_func_np(a))
-    apt = torch.tensor(a, requires_grad=True)
-    op_func_pt = getattr(apt, op_name)
-    spt = op_func_pt()
-    spt.sum().backward()
-    assert np.array_equal(apt.grad.numpy(), at.grad)
+        assert np.allclose(bpt.grad.numpy(), bt.grad, rtol=1e-15, atol=1e-20)
 
 
 def test_add():
@@ -74,29 +59,94 @@ def test_pow():
     helper_test_binary_op("__pow__", b=3.0)
 
 
-def test_matmul():
+def test_neg():
     a = np.random.random([2, 3, 4])
-    b = np.random.random([2, 3, 4])
-    op_func_np = getattr(a, op_name)
     at = nn.Tensor(a)
-    bt = nn.Tensor(b) if isinstance(b, np.ndarray) else b
-    op_func = getattr(at, op_name)
-    st = op_func(bt)
+    st = -at
     st.backward()
-    assert np.array_equal(st.value, op_func_np(b))
+    assert np.array_equal(st.value, -a)
     apt = torch.tensor(a, requires_grad=True)
-    bpt = torch.tensor(b, requires_grad=True) if isinstance(b, np.ndarray) else b
-    op_func_pt = getattr(apt, op_name)
-    spt = op_func_pt(bpt)
+    spt = -apt
     spt.sum().backward()
     assert np.array_equal(apt.grad.numpy(), at.grad)
-    if isinstance(b, np.ndarray):
-        assert np.array_equal(bpt.grad.numpy(), bt.grad)
+
+
+def test_radd():
+    helper_test_binary_op("__radd__")
+
+
+def test_sub():
+    helper_test_binary_op("__sub__")
+
+
+def test_rsub():
+    helper_test_binary_op("__rsub__")
+
+
+def test_rmul():
+    helper_test_binary_op("__rmul__")
 
 
 def test_log():
     helper_test_unary_op("log")
 
 
+def test_truediv():
+    helper_test_binary_op("__truediv__")
+
+
+def test_rtruediv():
+    helper_test_binary_op("__rtruediv__")
+
+
 def test_exp():
-    helper_test_unary_op("exp")
+    a = np.random.random([2, 3, 4])
+    at = nn.Tensor(a)
+    st = at.exp()
+    st.backward()
+    assert np.allclose(st.value, np.exp(a), rtol=1e-15, atol=1e-20)
+    apt = torch.tensor(a, requires_grad=True)
+    spt = apt.exp()
+    spt.sum().backward()
+    assert np.allclose(apt.grad.numpy(), at.grad, rtol=1e-15, atol=1e-20)
+
+
+def test_log():
+    a = np.random.random([2, 3, 4])
+    at = nn.Tensor(a)
+    st = at.log()
+    st.backward()
+    assert np.allclose(st.value, np.log(a), rtol=1e-15, atol=1e-20)
+    apt = torch.tensor(a, requires_grad=True)
+    spt = apt.log()
+    spt.sum().backward()
+    assert np.allclose(apt.grad.numpy(), at.grad, rtol=1e-15, atol=1e-20)
+
+
+def test_matmul():
+    a = np.random.random([2, 12])
+    b = np.random.random([12, 2])
+    at = nn.Tensor(a)
+    bt = nn.Tensor(b) if isinstance(b, np.ndarray) else b
+    st = at.matmul(bt)
+    st.backward()
+    assert np.array_equal(st.value, np.matmul(a, b))
+    assert st.value.shape == (2, 2)
+    apt = torch.tensor(a, requires_grad=True)
+    bpt = torch.tensor(b, requires_grad=True) if isinstance(b, np.ndarray) else b
+    spt = torch.matmul(apt, bpt)
+    spt.sum().backward()
+    assert np.array_equal(apt.grad.numpy(), at.grad)
+    assert np.array_equal(bpt.grad.numpy(), bt.grad)
+
+
+def test_relu():
+    a = np.random.random([2, 3, 4])
+    at = nn.Tensor(a)
+    st = at.relu()
+    st.backward()
+    assert np.array_equal(st.value, np.maximum(0, a))
+    apt = torch.tensor(a, requires_grad=True)
+    spt = torch.relu(apt)
+    spt.sum().backward()
+    assert np.array_equal(apt.grad.numpy(), at.grad)
